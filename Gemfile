ff40237ad11325d7a51fdbd129c708af2070805a
MikeTomato <mike@costabellarealtygroup.com>
2019-04-11 12:36:16 -0400

    added apps scaffold, added 10 apps to test api, added pagination in apps_controller

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby '2.6.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
+gem 'rb-readline'
+gem 'will_paginate-bootstrap4'
+gem 'will_paginate', '~> 3.1', '>= 3.1.6'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
---
+++ app/controllers/apps_controller.rb
@@ -0,0 +1,88 @@
+class AppsController < ApplicationController
+  before_action :set_app, only: [:show, :edit, :update, :destroy]
+
+  # GET /apps
+  # GET /apps.json
+  def index
+    @apps = App.all
+
+    respond_to do |format|
+       format.html do
+         @app = App.new
+       end
+       format.json do
+         apps        = (params[:name]) ? App.search(params[:name]) : App.all
+         page        = (params[:page] || 1).to_i
+         per_page    = 5
+         total_pages = (App.count.to_f / per_page).ceil
+         total_pages = 1 if total_pages.zero?
+         @appss      = App.paginate(page: page, per_page: per_page)
+         render json: { apps: @appss, page: page, totalPages: total_pages }
+  end
+ end
+end
+
+  # GET /apps/1
+  # GET /apps/1.json
+  def show
+  end
+
+  # GET /apps/new
+  def new
+    @app = App.new
+  end
+
+  # GET /apps/1/edit
+  def edit
+  end
+
+  # POST /apps
+  # POST /apps.json
+  def create
+    @app = App.new(app_params)
+
+    respond_to do |format|
+      if @app.save
+        format.html { redirect_to @app, notice: 'App was successfully created.' }
+        format.json { render :show, status: :created, location: @app }
+      else
+        format.html { render :new }
+        format.json { render json: @app.errors, status: :unprocessable_entity }
+      end
+    end
+end
+  # PATCH/PUT /apps/1
+  # PATCH/PUT /apps/1.json
+  def update
+    respond_to do |format|
+      if @app.update(app_params)
+        format.html { redirect_to @app, notice: 'App was successfully updated.' }
+        format.json { render :show, status: :ok, location: @app }
+      else
+        format.html { render :edit }
+        format.json { render json: @app.errors, status: :unprocessable_entity }
+      end
+    end
+end
+
+  # DELETE /apps/1
+  # DELETE /apps/1.json
+  def destroy
+    @app.destroy
+    respond_to do |format|
+      format.html { redirect_to apps_url, notice: 'App was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_app
+      @app = App.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def app_params
+      params.require(:app).permit(:name)
+    end
+end
